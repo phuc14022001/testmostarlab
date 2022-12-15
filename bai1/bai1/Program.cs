ef90acdd3d5ecc0751f2f003e1dea1c2991f839a
phuc14022001 <106235647+phuc14022001@users.noreply.github.com>
2022-12-15 16:21:14 +0700

    test

---
+++ bai1/bai1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace bai1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.WriteLine("nhap so a :");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("nhap so b :");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("nhap so c :");
+            c = int.Parse(Console.ReadLine());
+            double delta;
+            delta = b * b - 4 * c * a;
+            if (delta >= 0)
+            {
+                Console.WriteLine("căn delta = " + Math.Sqrt(delta));
+            }
+            else
+            {
+                Console.WriteLine("delta âm ");
+            }
+        }
+    }   
+}
+
---
+++ bai3/bai3/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+class Square
+{
+    public float canh { get; set; }
+    public Square() { }
+    public Square(float canh_)
+    {
+        this.canh = canh_;
+    }
+    public void nhap()
+    {
+        Console.Write("nhap do dai canh :");
+        canh = float.Parse(Console.ReadLine()); 
+    }
+    public void xuat()
+    {
+        Console.WriteLine("so do cua canh la :" + canh);
+    }
+    public void chuvi()
+    {
+        Console.WriteLine("chu vi =" + canh * 4);
+    }
+    public void dientich()
+    {
+        Console.WriteLine("dien tich =" + canh * canh);
+    }
+}
+
+namespace bai3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Square a=new Square();
+            a.nhap();
+            Console.WriteLine("chon 1 hien thi do dai canh.");
+            Console.WriteLine("chon 2 hien thi chu vi.");
+            Console.WriteLine("chon 3 hien thi dien tich.");
+            Console.WriteLine("chon 4 hien thi tat ca.");
+            Console.Write("nhap so ban muon chon :");
+            int choo;
+            choo = int.Parse(Console.ReadLine());
+            switch (choo)
+            {
+                case 1:
+                    a.xuat();
+                    break;
+                case 2:
+                    a.chuvi();
+                    break;
+                case 3:
+                    a.dientich();
+                    break;
+                case 4:
+                    a.xuat();
+                    a.chuvi();
+                    a.dientich();
+                    break;
+                case 5:
+                    Console.WriteLine("ket thuc !!!");
+                    break;  
+            }
+        }
+    }
+}
